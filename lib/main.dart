486f74d1d13bead0afab8798d312a1a1bf5c9863
reza <moradzade.mohamadreza@gmail.com>
2025-05-29 13:18:03 -0700

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:todo_app_with_getx/routes/routes.dart';
+
+import 'bindings/bindings.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      defaultTransition: Transition.fadeIn,
+      getPages: Routes.pages,
+      initialBinding: MyBindings(),
+    );
+  }
 }
